d96a5cf34acf5f894fafb74d488e1db8ade3755f
Inneska13 <innaivanchuk1313@gmail.com>
2023-08-09 23:43:42 +0300

    create user crud

--- src/route/index.js
+++ src/route/index.js
@@ -14,16 +14,43 @@ class User {
     this.id = new Date().getTime()
   }
 
+  verifyPassword = (password) => this.password === password
+
   static add = (user) => {
     this.#list.push(user)
   }
 
-  static getList = () => {
-    this.#list
+  static getList = () => this.#list
+
+  static getById = (id) =>
+    this.#list.find((user) => user.id === id)
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (user) => user.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+  static updateById = (id, data) => {
+    const user = this.getById(id)
+
+    if (user) {
+      this.update(user, data)
+      return true
+    } else {
+      return false
+    }
   }
 
-  static getById = (id) => {
-    this.#list.find((user) => user.id === id)
+  static update = (user, { email }) => {
+    if (email) {
+      user.email = email
+    }
   }
 }
 
@@ -71,19 +98,34 @@ router.post('/user-create', function (req, res) {
 router.get('/user-delete', function (req, res) {
   const { id } = req.query
 
-  console.log(typeof id)
-
-  const user = User.getById(Number(id))
-
-  console.log(user)
-
-  if (user) {
-    console.log('!!!!!!!!!!!!!!!!!!!!!!!!')
-  }
+  User.getById(Number(id))
 
   res.render('success-info', {
     style: 'success-info',
     info: 'Користувач видалений',
+  })
+})
+//===========================
+
+// ================================================================
+
+router.post('/user-update', function (req, res) {
+  const { email, password, id } = req.body
+
+  let result = false
+
+  const user = User.getById(Number(id))
+
+  if (user.verifyPassword(password)) {
+    User.update(user, { email })
+    result = true
+  }
+
+  res.render('success-info', {
+    style: 'success-info',
+    info: result
+      ? 'Email пошта оновлена'
+      : 'Cталася помилкка',
   })
 })
 //===========================
