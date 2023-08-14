32ef838d49c47a458882a57e013e3ffd16ec567d
Inneska13 <innaivanchuk1313@gmail.com>
2023-08-14 13:19:29 +0300

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -4,93 +4,43 @@ const express = require('express')
 const router = express.Router()
 
 // ================================================================
-
-class User {
-  static #list = []
-  constructor(email, login, password) {
-    this.email = email
-    this.login = login
-    this.password = password
-    this.id = new Date().getTime()
-  }
-
-  verifyPassword = (password) => this.password === password
-
-  static add = (user) => {
-    this.#list.push(user)
-  }
-
-  static getList = () => this.#list
-
-  static getById = (id) =>
-    this.#list.find((user) => user.id === id)
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (user) => user.id === id,
-    )
-    if (index !== -1) {
-      this.#list.splice(index, 1)
-      return true
-    } else {
-      return false
-    }
-  }
-  static updateById = (id, data) => {
-    const user = this.getById(id)
-
-    if (user) {
-      this.update(user, data)
-      return true
-    } else {
-      return false
-    }
-  }
-
-  static update = (user, { email }) => {
-    if (email) {
-      user.email = email
-    }
-  }
-}
-
 // ================================================================
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
-  const list = User.getList()
-  // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
-    data: {
-      users: {
-        list,
-        isEmpty: list.length === 0,
-      },
-    },
-  })
-  // ↑↑ сюди вводимо JSON дані
-})
+// router.get('/', function (req, res) {
+//   // res.render генерує нам HTML сторінку
+//   const list = User.getList()
+//   // ↙️ cюди вводимо назву файлу з сontainer
+//   res.render('index', {
+//     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+//     style: 'index',
+//     data: {
+//       users: {
+//         list,
+//         isEmpty: list.length === 0,
+//       },
+//     },
+//   })
+//   // ↑↑ сюди вводимо JSON дані
+// })
 
 // ================================================================
 
-router.post('/user-create', function (req, res) {
-  const { email, login, password } = req.body
+// router.post('/user-create', function (req, res) {
+//   const { email, login, password } = req.body
 
-  const user = new User(email, login, password)
+//   const user = new User(email, login, password)
 
-  User.add(user)
+//   User.add(user)
 
-  console.log(User.getList())
+//   console.log(User.getList())
 
-  res.render('success-info', {
-    style: 'success-info',
-    info: 'Користувач створений',
-  })
-})
+//   res.render('success-info', {
+//     style: 'success-info',
+//     info: 'Користувач створений',
+//   })
+// })
 //===========================
 
 // ================================================================
@@ -139,7 +89,10 @@ class Product {
     this.name = name
     this.price = price
     this.description = description
-    this.createDate = new Date().createDate(createDate)
+
+    this.createDate = () => {
+      this.date = new Date().toISOString()
+    }
   }
 
   verifyPrice = (price) => this.price === price
